ea831ac261c2b3376c6fee1ff16f0525abfa578e
Alexandre Champagne <achampagnedev@gmail.com>
2022-10-17 22:50:18 +0800

    Updated libs to beta.4

---
+++ traits.ts
@@ -0,0 +1,9 @@
+enum Trait {
+  Category = 'Category',
+}
+
+export const keys = [Trait.Category]
+
+export const values = {
+  [Trait.Category]: ['Art', 'Photography', 'Currency', 'DeFi', 'Games'],
+}
