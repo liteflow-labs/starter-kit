fda5f47f95d51aa6fc0d53f598582afc4c07b8f1
antho1404 <anthony.estebe@gmail.com>
2022-11-01 22:01:48 +0700

    integrate chat system in application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import { EmailConnector } from '@nft/email-connector'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
+import ChatWindow from 'components/ChatWindow'
 import dayjs from 'dayjs'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
@@ -153,37 +154,39 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         bugsnagAPIKey={environment.BUGSNAG_API_KEY}
         theme={theme}
       >
-        <Box mt={12}>
-          <Banner />
-          <Navbar
-            allowTopUp={true}
-            router={{
-              asPath: router.asPath,
-              isReady: router.isReady,
-              push: router.push,
-              query: router.query,
-              events: router.events,
-            }}
-            login={{
-              email: true,
-              metamask: true,
-              walletConnect: true,
-              coinbase: true,
-              networkName: environment.NETWORK_NAME,
-            }}
-            multiLang={{
-              locale: router.locale,
-              pathname: router.pathname,
-              choices: [
-                { label: 'En', value: 'en' },
-                { label: '日本語', value: 'ja' },
-                { label: '中文', value: 'zh-cn' },
-              ],
-            }}
-          />
-          <Component {...pageProps} />
-          <Footer name="Acme, Inc." links={footerLinks} />
-        </Box>
+        <ChatWindow>
+          <Box mt={12}>
+            <Banner />
+            <Navbar
+              allowTopUp={true}
+              router={{
+                asPath: router.asPath,
+                isReady: router.isReady,
+                push: router.push,
+                query: router.query,
+                events: router.events,
+              }}
+              login={{
+                email: true,
+                metamask: true,
+                walletConnect: true,
+                coinbase: true,
+                networkName: environment.NETWORK_NAME,
+              }}
+              multiLang={{
+                locale: router.locale,
+                pathname: router.pathname,
+                choices: [
+                  { label: 'En', value: 'en' },
+                  { label: '日本語', value: 'ja' },
+                  { label: '中文', value: 'zh-cn' },
+                ],
+              }}
+            />
+            <Component {...pageProps} />
+            <Footer name="Acme, Inc." links={footerLinks} />
+          </Box>
+        </ChatWindow>
       </LiteflowNFTApp>
     </>
   )
