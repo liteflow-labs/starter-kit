ceed21162829064575ef01da80f5d37c23c7557c
antho1404 <anthony.estebe@gmail.com>
2022-12-05 10:48:40 +0700

    move account pages from template to starter kit

--- pages/account/edit.tsx
+++ pages/account/edit.tsx
@@ -1,22 +1,59 @@
-import { UserForm } from '@nft/templates'
+import { useToast } from '@chakra-ui/react'
+import { Account, UserFormEdit } from '@nft/components'
+import { useWeb3React } from '@web3-react/core'
 import { NextPage } from 'next'
+import useTranslation from 'next-translate/useTranslation'
+import { useRouter } from 'next/router'
+import { useCallback } from 'react'
 import Head from '../../components/Head'
 import environment from '../../environment'
+import { useGetAccountQuery } from '../../graphql'
 import useEagerConnect from '../../hooks/useEagerConnect'
+import useLoginRedirect from '../../hooks/useLoginRedirect'
 import useSigner from '../../hooks/useSigner'
 import SmallLayout from '../../layouts/small'
 
 const EditPage: NextPage = () => {
   const ready = useEagerConnect()
   const signer = useSigner()
+  const { t } = useTranslation('templates')
+  const { push } = useRouter()
+  const { account } = useWeb3React()
+  useLoginRedirect(ready)
+
+  const toast = useToast()
+
+  const { data } = useGetAccountQuery({
+    variables: {
+      address: account?.toLowerCase() || '',
+    },
+    skip: !account,
+  })
+
+  const onSubmit = useCallback(
+    async (address: string) => {
+      toast({
+        title: t('users.form.notifications.updated'),
+        status: 'success',
+      })
+      await push(`/users/${address}`)
+    },
+    [toast, t, push],
+  )
+
+  if (!data?.account) return <></>
   return (
     <SmallLayout>
       <Head title="Account - Edit profile" />
-      <UserForm.Template
-        uploadUrl={environment.UPLOAD_URL}
-        ready={ready}
-        signer={signer}
-      />
+
+      <Account currentTab="edit-profile">
+        <UserFormEdit
+          signer={signer}
+          onUpdated={onSubmit}
+          uploadUrl={environment.UPLOAD_URL}
+          account={data.account}
+        />
+      </Account>
     </SmallLayout>
   )
 }
