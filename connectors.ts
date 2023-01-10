86b831322e88143829c98a70475ae6fb8f207d6e
Michael McClenaghan <michael@defylabs.xyz>
2023-01-10 19:17:38 +1300

    fix: comment out email connector

--- connectors.ts
+++ connectors.ts
@@ -1,4 +1,4 @@
-import { EmailConnector } from '@nft/email-connector'
+// import { EmailConnector } from '@nft/email-connector'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
