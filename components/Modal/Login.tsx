29d4e81ae429dfd29f8678cf5b89c42b69637e44
antho1404 <anthony.estebe@gmail.com>
2023-02-07 19:05:17 +0700

    merge login modal and page into a dedicated component

--- components/Modal/Login.tsx
+++ components/Modal/Login.tsx
@@ -1,6 +1,4 @@
 import {
-  Box,
-  Flex,
   Heading,
   Modal,
   ModalBody,
@@ -9,17 +7,12 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  Stack,
   Text,
 } from '@chakra-ui/react'
-import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core'
+import { useWeb3React } from '@web3-react/core'
 import useTranslation from 'next-translate/useTranslation'
-import { FC, useEffect, useMemo, useState } from 'react'
-import connectors from '../../connectors'
-import WalletCoinbase from '../Wallet/Connectors/Coinbase'
-import WalletEmail from '../Wallet/Connectors/Email'
-import WalletMetamask from '../Wallet/Connectors/Metamask'
-import WalletWalletConnect from '../Wallet/Connectors/WalletConnect'
+import { FC, useEffect } from 'react'
+import LoginForm from '../Login/Form'
 
 type Props = {
   isOpen: boolean
@@ -29,19 +22,7 @@ type Props = {
 
 const LoginModal: FC<Props> = ({ isOpen, onClose, networkName }) => {
   const { t } = useTranslation('components')
-  const { account, error, activate } = useWeb3React()
-  const [errorFromLogin, setErrorFromLogin] = useState<Error>()
-
-  const invalidNetwork = useMemo(
-    () => errorFromLogin && errorFromLogin instanceof UnsupportedChainIdError,
-    [errorFromLogin],
-  )
-
-  const hasStandardWallet = useMemo(
-    () =>
-      connectors.injected || connectors.coinbase || connectors.walletConnect,
-    [],
-  )
+  const { account, activate } = useWeb3React()
 
   useEffect(() => {
     if (account) onClose()
@@ -62,106 +43,7 @@ const LoginModal: FC<Props> = ({ isOpen, onClose, networkName }) => {
             {t('modal.login.description')}
           </Text>
 
-          {connectors.email && <WalletEmail activate={activate} />}
-          {connectors.email && hasStandardWallet && (
-            <Box position="relative" mt={6} mb={2}>
-              <Flex
-                position="absolute"
-                align="center"
-                top={0}
-                right={0}
-                bottom={0}
-                left={0}
-              >
-                <Box w="full" borderTop="1px" borderColor="gray.200" />
-              </Flex>
-              <Flex position="relative" bgColor="white" pr={2}>
-                <Text
-                  as="p"
-                  variant="text-sm"
-                  fontWeight={500}
-                  color="gray.500"
-                >
-                  {t('modal.login.alternative')}
-                </Text>
-              </Flex>
-            </Box>
-          )}
-
-          {error && (
-            <Text as="span" role="alert" variant="error" mt={3}>
-              {error.message ? error.message : error.toString()}
-            </Text>
-          )}
-          {invalidNetwork && (
-            <Text as="span" role="alert" variant="error" mt={3}>
-              {t('modal.login.errors.wrong-network', { networkName })}
-            </Text>
-          )}
-          {hasStandardWallet && (
-            <Flex direction={{ base: 'column', md: 'row' }} gap={3}>
-              {connectors.injected && (
-                <Stack
-                  cursor="pointer"
-                  w="full"
-                  spacing={3}
-                  rounded="xl"
-                  borderWidth={1}
-                  borderColor="gray.200"
-                  shadow="sm"
-                  _hover={{
-                    shadow: 'md',
-                  }}
-                  transition="box-shadow 0.3s ease-in-out"
-                >
-                  <WalletMetamask
-                    activate={activate}
-                    onError={setErrorFromLogin}
-                  />
-                </Stack>
-              )}
-              {connectors.coinbase && (
-                <Stack
-                  cursor="pointer"
-                  w="full"
-                  spacing={3}
-                  rounded="xl"
-                  borderWidth={1}
-                  borderColor="gray.200"
-                  shadow="sm"
-                  _hover={{
-                    shadow: 'md',
-                  }}
-                  transition="box-shadow 0.3s ease-in-out"
-                >
-                  <WalletCoinbase
-                    activate={activate}
-                    onError={setErrorFromLogin}
-                  />
-                </Stack>
-              )}
-              {connectors.walletConnect && (
-                <Stack
-                  cursor="pointer"
-                  w="full"
-                  spacing={3}
-                  rounded="xl"
-                  borderWidth={1}
-                  borderColor="gray.200"
-                  shadow="sm"
-                  _hover={{
-                    shadow: 'md',
-                  }}
-                  transition="box-shadow 0.3s ease-in-out"
-                >
-                  <WalletWalletConnect
-                    activate={activate}
-                    onError={setErrorFromLogin}
-                  />
-                </Stack>
-              )}
-            </Flex>
-          )}
+          <LoginForm activate={activate} networkName={networkName} />
         </ModalBody>
         <ModalFooter as="div" />
       </ModalContent>
