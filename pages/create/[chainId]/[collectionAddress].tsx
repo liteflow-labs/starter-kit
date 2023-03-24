1f50a9b7eea07f00efb3a0c27624cb1481d55c58
ismail <ismailtoyran@gmail.com>
2023-03-24 13:16:57 +0300

    Add missing notFound

--- pages/create/[chainId]/[collectionAddress].tsx
+++ pages/create/[chainId]/[collectionAddress].tsx
@@ -82,6 +82,7 @@ export const getServerSideProps = wrapServerSideProps<Props>(
     })
     if (error) throw error
     if (!data) throw new Error('data is falsy')
+    if (!data.collection) return { notFound: true }
     return {
       props: {
         chainId: parseInt(chainId, 10),
