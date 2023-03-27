6a0ac28b568bd8a674da5679180dd5717f7f6db1
antho1404 <anthony.estebe@gmail.com>
2023-03-27 23:55:09 +0700

    update user fixed page with new ssr

--- pages/users/[id]/offers/fixed.tsx
+++ pages/users/[id]/offers/fixed.tsx
@@ -22,7 +22,6 @@ import Trans from 'next-translate/Trans'
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo } from 'react'
-import invariant from 'ts-invariant'
 import CancelOfferButton from '../../../../components/Button/CancelOffer'
 import Empty from '../../../../components/Empty/Empty'
 import Head from '../../../../components/Head'
@@ -34,72 +33,21 @@ import UserProfileTemplate from '../../../../components/Profile'
 import Select from '../../../../components/Select/Select'
 import { convertFullUser, convertSaleFull } from '../../../../convert'
 import environment from '../../../../environment'
-import {
-  FetchUserFixedPriceDocument,
-  FetchUserFixedPriceQuery,
-  OffersOrderBy,
-  useFetchUserFixedPriceQuery,
-} from '../../../../graphql'
+import { OffersOrderBy, useFetchUserFixedPriceQuery } from '../../../../graphql'
 import useAccount from '../../../../hooks/useAccount'
 import useEagerConnect from '../../../../hooks/useEagerConnect'
 import useOrderByQuery from '../../../../hooks/useOrderByQuery'
 import usePaginate from '../../../../hooks/usePaginate'
 import usePaginateQuery from '../../../../hooks/usePaginateQuery'
+import useRequiredQueryParamSingle from '../../../../hooks/useRequiredQueryParamSingle'
 import useSigner from '../../../../hooks/useSigner'
 import LargeLayout from '../../../../layouts/large'
-import { getLimit, getOffset, getOrder } from '../../../../params'
-import { wrapServerSideProps } from '../../../../props'
 
 type Props = {
-  userAddress: string
   now: string
-  meta: {
-    title: string
-    description: string
-    image: string
-  }
 }
 
-export const getServerSideProps = wrapServerSideProps<Props>(
-  environment.GRAPHQL_URL,
-  async (context, client) => {
-    const userAddress = context.params?.id
-      ? Array.isArray(context.params.id)
-        ? context.params.id[0]?.toLowerCase()
-        : context.params.id.toLowerCase()
-      : null
-    invariant(userAddress, 'userAddress is falsy')
-    const limit = getLimit(context, environment.PAGINATION_LIMIT)
-    const orderBy = getOrder<OffersOrderBy>(context, 'CREATED_AT_DESC')
-    const offset = getOffset(context, environment.PAGINATION_LIMIT)
-    const now = new Date()
-    const { data, error } = await client.query<FetchUserFixedPriceQuery>({
-      query: FetchUserFixedPriceDocument,
-      variables: {
-        limit,
-        offset,
-        orderBy,
-        address: userAddress,
-        now,
-      },
-    })
-    if (error) throw error
-    if (!data) throw new Error('data is falsy')
-    return {
-      props: {
-        userAddress,
-        now: now.toJSON(),
-        meta: {
-          title: data.account?.name || userAddress,
-          description: data.account?.description || '',
-          image: data.account?.image || '',
-        },
-      },
-    }
-  },
-)
-
-const FixedPricePage: NextPage<Props> = ({ meta, now, userAddress }) => {
+const FixedPricePage: NextPage<Props> = ({ now }) => {
   useEagerConnect()
   const signer = useSigner()
   const { t } = useTranslation('templates')
@@ -109,6 +57,7 @@ const FixedPricePage: NextPage<Props> = ({ meta, now, userAddress }) => {
   const orderBy = useOrderByQuery<OffersOrderBy>('CREATED_AT_DESC')
   const [changePage, changeLimit] = usePaginate()
   const toast = useToast()
+  const userAddress = useRequiredQueryParamSingle('id')
   const ownerLoggedIn = useIsLoggedIn(userAddress)
 
   const date = useMemo(() => new Date(now), [now])
@@ -156,9 +105,9 @@ const FixedPricePage: NextPage<Props> = ({ meta, now, userAddress }) => {
   return (
     <LargeLayout>
       <Head
-        title={meta.title}
-        description={meta.description}
-        image={meta.image}
+        title={userAccount?.name || userAddress}
+        description={userAccount?.description || ''}
+        image={userAccount?.image || ''}
       />
       <UserProfileTemplate
         signer={signer}
