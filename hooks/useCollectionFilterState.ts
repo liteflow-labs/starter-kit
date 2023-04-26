2beb00c9c4e496c27fc04e313eb179746450d48d
antho1404 <anthony.estebe@gmail.com>
2023-04-27 00:30:36 +0700

    add collection filter

---
+++ hooks/useCollectionFilterState.ts
@@ -0,0 +1,27 @@
+import { useBreakpointValue } from '@chakra-ui/react'
+import { useEffect, useMemo, useState } from 'react'
+import { Filter } from './useCollectionFilterFromQuery'
+
+export default function useCollectionFilterState(filter: Filter): {
+  showFilters: boolean
+  toggleFilters: () => void
+  close: () => void
+  count: number
+} {
+  const display = useBreakpointValue({ base: false, md: true }) || false
+  const [showFilters, setShowFilters] = useState(display)
+  const filterCount = useMemo(() => {
+    let count = 0
+    if (filter.chains.length > 0) count += 1
+    return count
+  }, [filter])
+
+  useEffect(() => setShowFilters(display), [display, setShowFilters])
+
+  return {
+    showFilters: showFilters,
+    toggleFilters: () => setShowFilters((x) => !x),
+    close: () => setShowFilters(false),
+    count: filterCount,
+  }
+}
