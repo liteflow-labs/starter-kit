eb3af9a21946182629451bfc9129d79d01918ee3
antho1404 <anthony.estebe@gmail.com>
2023-04-28 18:59:50 +0700

    add skeleton to onsale page

--- pages/users/[id]/on-sale.tsx
+++ pages/users/[id]/on-sale.tsx
@@ -4,14 +4,13 @@ import Trans from 'next-translate/Trans'
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo } from 'react'
-import Head from '../../../components/Head'
-import Loader from '../../../components/Loader'
 import UserProfileTemplate from '../../../components/Profile'
+import SkeletonGrid from '../../../components/Skeleton/Grid'
+import SkeletonTokenCard from '../../../components/Skeleton/TokenCard'
 import TokenGrid from '../../../components/Token/Grid'
 import {
   convertAsset,
   convertAuctionWithBestBid,
-  convertFullUser,
   convertSale,
   convertUser,
 } from '../../../convert'
@@ -57,11 +56,6 @@ const OnSalePage: NextPage<Props> = ({ now }) => {
     },
   })
 
-  const userAccount = useMemo(
-    () => convertFullUser(data?.account || null, userAddress),
-    [data, userAddress],
-  )
-
   const changeOrder = useCallback(
     async (orderBy: any) => {
       await replace({ pathname, query: { ...query, orderBy } })
@@ -90,71 +84,62 @@ const OnSalePage: NextPage<Props> = ({ now }) => {
     [data],
   )
 
-  if (loading) return <Loader fullPage />
-  if (!assets) return <></>
-  if (!data) return <></>
   return (
     <LargeLayout>
-      <Head
-        title={data.account?.name || userAddress}
-        description={data.account?.description || ''}
-        image={data.account?.image || ''}
-      />
-
       <UserProfileTemplate
+        now={date}
         signer={signer}
         currentAccount={address}
-        account={userAccount}
+        address={userAddress}
         currentTab="on-sale"
-        totals={
-          new Map([
-            ['created', data.created?.totalCount || 0],
-            ['on-sale', data.onSale?.totalCount || 0],
-            ['owned', data.owned?.totalCount || 0],
-          ])
-        }
         loginUrlForReferral={environment.BASE_URL + '/login'}
       >
-        <TokenGrid<AssetsOrderBy>
-          assets={assets}
-          orderBy={{
-            value: orderBy,
-            choices: [
-              {
-                label: t('user.on-sale-assets.orderBy.values.createdAtDesc'),
-                value: 'CREATED_AT_DESC',
+        {loading ? (
+          <SkeletonGrid items={environment.PAGINATION_LIMIT} compact>
+            <SkeletonTokenCard />
+          </SkeletonGrid>
+        ) : (
+          <TokenGrid<AssetsOrderBy>
+            assets={assets}
+            orderBy={{
+              value: orderBy,
+              choices: [
+                {
+                  label: t('user.on-sale-assets.orderBy.values.createdAtDesc'),
+                  value: 'CREATED_AT_DESC',
+                },
+                {
+                  label: t('user.on-sale-assets.orderBy.values.createdAtAsc'),
+                  value: 'CREATED_AT_ASC',
+                },
+              ],
+              onSort: changeOrder,
+            }}
+            pagination={{
+              limit,
+              limits: [environment.PAGINATION_LIMIT, 24, 36, 48],
+              page,
+              total: data?.onSale?.totalCount || 0,
+              onPageChange: changePage,
+              onLimitChange: changeLimit,
+              result: {
+                label: t('pagination.result.label'),
+                caption: (props) => (
+                  <Trans
+                    ns="templates"
+                    i18nKey="pagination.result.caption"
+                    values={props}
+                    components={[
+                      <Text as="span" color="brand.black" key="text" />,
+                    ]}
+                  />
+                ),
+                pages: (props) =>
+                  t('pagination.result.pages', { count: props.total }),
               },
-              {
-                label: t('user.on-sale-assets.orderBy.values.createdAtAsc'),
-                value: 'CREATED_AT_ASC',
-              },
-            ],
-            onSort: changeOrder,
-          }}
-          pagination={{
-            limit,
-            limits: [environment.PAGINATION_LIMIT, 24, 36, 48],
-            page,
-            total: data.onSale?.totalCount || 0,
-            onPageChange: changePage,
-            onLimitChange: changeLimit,
-            result: {
-              label: t('pagination.result.label'),
-              caption: (props) => (
-                <Trans
-                  ns="templates"
-                  i18nKey="pagination.result.caption"
-                  values={props}
-                  components={[
-                    <Text as="span" color="brand.black" key="text" />,
-                  ]}
-                />
-              ),
-              pages: (props) =>
-                t('pagination.result.pages', { count: props.total }),
-            },
-          }}
-        />
+            }}
+          />
+        )}
       </UserProfileTemplate>
     </LargeLayout>
   )
