0291ee91c50ca0ffdf313d848b2bb29704b81539
ismail <ismailtoyran@gmail.com>
2023-05-10 13:50:52 +0300

    Users added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import AssetsHomeSection from 'components/HomeSection/Assets'
 import AuctionsHomeSection from 'components/HomeSection/Auctions'
 import CollectionsHomeSection from 'components/HomeSection/Collections'
 import FeaturedHomeSection from 'components/HomeSection/Featured'
+import UsersHomeSection from 'components/HomeSection/Users'
 import { NextPage } from 'next'
 import { useMemo } from 'react'
 import LargeLayout from '../layouts/large'
@@ -17,6 +18,7 @@ const HomePage: NextPage<Props> = ({ now }) => {
       <Stack spacing={12}>
         <FeaturedHomeSection date={date} />
         <CollectionsHomeSection />
+        <UsersHomeSection />
         <AuctionsHomeSection date={date} />
         <AssetsHomeSection date={date} />
       </Stack>
