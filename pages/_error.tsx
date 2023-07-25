a1c324b5af933cf0cb9f8b8d53c1a0bdf00da4e0
ismail <ismailtoyran@gmail.com>
2023-07-25 10:37:47 +0300

    New design and locales added

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -3,6 +3,8 @@ import { MdError } from '@react-icons/all-files/md/MdError'
 import Empty from 'components/Empty/Empty'
 import environment from 'environment'
 import { NextPage, NextPageContext } from 'next'
+import useTranslation from 'next-translate/useTranslation'
+import { useMemo } from 'react'
 import Head from '../components/Head'
 import SmallLayout from '../layouts/small'
 
@@ -11,18 +13,27 @@ interface Props {
 }
 
 const Error: NextPage<Props> = ({ statusCode }) => {
+  const { t } = useTranslation('templates')
+  const content = useMemo(() => {
+    if (statusCode === 404) {
+      return {
+        title: t('error.404.title'),
+        description: t('error.404.description'),
+      }
+    }
+    return {
+      title: t('error.500.title'),
+      description: t('error.500.description'),
+    }
+  }, [statusCode, t])
   return (
     <SmallLayout>
-      <Head title={`${statusCode ? `${statusCode.toString()} ` : ''}Error`} />
+      <Head title={`${statusCode ? `${statusCode} ` : ''}Error`} />
       <Empty
         icon={<Icon as={MdError} color="red.500" h={9} w={9} />}
-        title="Error occurred"
-        description={
-          statusCode
-            ? `An error ${statusCode} occurred on server`
-            : 'An error occurred on client'
-        }
-        button="Contact"
+        title={content.title}
+        description={content.description}
+        button={t('error.button')}
         href={`mailto:${environment.REPORT_EMAIL}`}
         isExternal
       />
