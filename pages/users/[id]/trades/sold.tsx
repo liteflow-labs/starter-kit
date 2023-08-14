e6284da5a5c4505d06d514dd433d23a4bbc1ca5e
ismail <ismailtoyran@gmail.com>
2023-08-14 19:11:32 +0300

    Users sold page pagination changes

--- pages/users/[id]/trades/sold.tsx
+++ pages/users/[id]/trades/sold.tsx
@@ -17,7 +17,6 @@ import {
 import { HiExternalLink } from '@react-icons/all-files/hi/HiExternalLink'
 import { HiOutlineSearch } from '@react-icons/all-files/hi/HiOutlineSearch'
 import { NextPage } from 'next'
-import Trans from 'next-translate/Trans'
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo } from 'react'
@@ -58,11 +57,7 @@ const TradeSoldPage: NextPage<Props> = ({ now }) => {
   const userAddress = useRequiredQueryParamSingle('id')
 
   const date = useMemo(() => new Date(now), [now])
-  const {
-    data,
-    loading: _loading,
-    previousData,
-  } = useFetchUserTradeSoldQuery({
+  const { data } = useFetchUserTradeSoldQuery({
     variables: {
       address: userAddress,
       limit,
@@ -71,6 +66,11 @@ const TradeSoldPage: NextPage<Props> = ({ now }) => {
     },
   })
 
+  const trades = useMemo(
+    () => (data?.trades?.nodes || []).map(convertTrade),
+    [data?.trades?.nodes],
+  )
+
   const changeOrder = useCallback(
     async (orderBy: any) => {
       await replace({ pathname, query: { ...query, orderBy } })
@@ -78,12 +78,15 @@ const TradeSoldPage: NextPage<Props> = ({ now }) => {
     [replace, pathname, query],
   )
 
-  const tradeData = useMemo(() => data || previousData, [data, previousData])
-  const trades = useMemo(
-    () => (tradeData?.trades?.nodes || []).map(convertTrade),
-    [tradeData],
+  const hasNextPage = useMemo(
+    () => data?.trades?.pageInfo.hasNextPage,
+    [data?.trades?.pageInfo.hasNextPage],
   )
-  const loading = _loading && !tradeData
+
+  const hasPreviousPage = useMemo(
+    () => data?.trades?.pageInfo.hasPreviousPage,
+    [data?.trades?.pageInfo.hasPreviousPage],
+  )
 
   return (
     <LargeLayout>
@@ -163,15 +166,9 @@ const TradeSoldPage: NextPage<Props> = ({ now }) => {
             </Box>
           </Flex>
 
-          {loading ? (
+          {data === undefined ? (
             <Loader />
-          ) : trades.length == 0 ? (
-            <Empty
-              icon={<Icon as={HiOutlineSearch} w={8} h={8} color="gray.400" />}
-              title={t('user.trade-sold.table.empty.title')}
-              description={t('user.trade-sold.table.empty.description')}
-            />
-          ) : (
+          ) : data.trades && data.trades.nodes.length > 0 ? (
             <TableContainer bg="white" shadow="base" rounded="lg">
               <Table>
                 <Thead>
@@ -271,31 +268,21 @@ const TradeSoldPage: NextPage<Props> = ({ now }) => {
                 </Tbody>
               </Table>
             </TableContainer>
+          ) : (
+            <Empty
+              icon={<Icon as={HiOutlineSearch} w={8} h={8} color="gray.400" />}
+              title={t('user.trade-sold.table.empty.title')}
+              description={t('user.trade-sold.table.empty.description')}
+            />
           )}
-
           <Pagination
             limit={limit}
             limits={[environment.PAGINATION_LIMIT, 24, 36, 48]}
-            onLimitChange={changeLimit}
-            onPageChange={changePage}
             page={page}
-            total={tradeData?.trades?.totalCount}
-            isLoading={loading}
-            result={{
-              label: t('pagination.result.label'),
-              caption: (props) => (
-                <Trans
-                  ns="templates"
-                  i18nKey="pagination.result.caption"
-                  values={props}
-                  components={[
-                    <Text as="span" color="brand.black" key="text" />,
-                  ]}
-                />
-              ),
-              pages: (props) =>
-                t('pagination.result.pages', { count: props.total }),
-            }}
+            onPageChange={changePage}
+            onLimitChange={changeLimit}
+            hasNextPage={hasNextPage}
+            hasPreviousPage={hasPreviousPage}
           />
         </Stack>
       </UserProfileTemplate>
