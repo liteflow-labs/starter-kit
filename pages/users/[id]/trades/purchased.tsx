3b77dd48269976a2763d48c2bf6798b8c220438e
ismail <ismailtoyran@gmail.com>
2023-08-14 19:11:26 +0300

    Users purchased page pagination changes

--- pages/users/[id]/trades/purchased.tsx
+++ pages/users/[id]/trades/purchased.tsx
@@ -17,7 +17,6 @@ import {
 import { HiExternalLink } from '@react-icons/all-files/hi/HiExternalLink'
 import { HiOutlineSearch } from '@react-icons/all-files/hi/HiOutlineSearch'
 import { NextPage } from 'next'
-import Trans from 'next-translate/Trans'
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo } from 'react'
@@ -61,11 +60,7 @@ const TradePurchasedPage: NextPage<Props> = ({ now }) => {
   const userAddress = useRequiredQueryParamSingle('id')
 
   const date = useMemo(() => new Date(now), [now])
-  const {
-    data,
-    loading: _loading,
-    previousData,
-  } = useFetchUserTradePurchasedQuery({
+  const { data } = useFetchUserTradePurchasedQuery({
     variables: {
       address: userAddress,
       limit,
@@ -74,18 +69,26 @@ const TradePurchasedPage: NextPage<Props> = ({ now }) => {
     },
   })
 
-  const tradeData = useMemo(() => data || previousData, [data, previousData])
   const trades = useMemo(
-    () => (tradeData?.trades?.nodes || []).map(convertTrade),
-    [tradeData],
+    () => (data?.trades?.nodes || []).map(convertTrade),
+    [data?.trades?.nodes],
   )
-  const loading = _loading && !tradeData
 
   const changeOrder = useCallback(
     async (orderBy: any) => {
       await replace({ pathname, query: { ...query, orderBy } })
     },
     [replace, pathname, query],
+  )
+
+  const hasNextPage = useMemo(
+    () => data?.trades?.pageInfo.hasNextPage,
+    [data?.trades?.pageInfo.hasNextPage],
+  )
+
+  const hasPreviousPage = useMemo(
+    () => data?.trades?.pageInfo.hasPreviousPage,
+    [data?.trades?.pageInfo.hasPreviousPage],
   )
 
   return (
@@ -170,15 +173,9 @@ const TradePurchasedPage: NextPage<Props> = ({ now }) => {
             </Box>
           </Flex>
 
-          {loading ? (
+          {data === undefined ? (
             <Loader />
-          ) : trades.length == 0 ? (
-            <Empty
-              icon={<Icon as={HiOutlineSearch} w={8} h={8} color="gray.400" />}
-              title={t('user.trade-purchased.table.empty.title')}
-              description={t('user.trade-purchased.table.empty.description')}
-            />
-          ) : (
+          ) : data.trades && data.trades.nodes.length > 0 ? (
             <TableContainer bg="white" shadow="base" rounded="lg">
               <Table>
                 <Thead>
@@ -278,31 +275,21 @@ const TradePurchasedPage: NextPage<Props> = ({ now }) => {
                 </Tbody>
               </Table>
             </TableContainer>
+          ) : (
+            <Empty
+              icon={<Icon as={HiOutlineSearch} w={8} h={8} color="gray.400" />}
+              title={t('user.trade-purchased.table.empty.title')}
+              description={t('user.trade-purchased.table.empty.description')}
+            />
           )}
-
           <Pagination
             limit={limit}
             limits={[environment.PAGINATION_LIMIT, 24, 36, 48]}
-            onLimitChange={changeLimit}
-            onPageChange={changePage}
             page={page}
-            total={tradeData?.trades?.totalCount}
-            isLoading={loading}
-            result={{
-              label: t('pagination.result.label'),
-              caption: (props) => (
-                <Trans
-                  ns="templates"
-                  i18nKey="pagination.result.caption"
-                  values={props}
-                  components={[
-                    <Text as="span" color="brand.black" key="text" />,
-                  ]}
-                />
-              ),
-              pages: (props) =>
-                t('pagination.result.pages', { count: props.total }),
-            }}
+            onPageChange={changePage}
+            onLimitChange={changeLimit}
+            hasNextPage={hasNextPage}
+            hasPreviousPage={hasPreviousPage}
           />
         </Stack>
       </UserProfileTemplate>
