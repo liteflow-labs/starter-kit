91b43dc421bf0a7f87846fa4546beb6892822607
ismail <ismailtoyran@gmail.com>
2023-08-14 19:10:52 +0300

    Users placed page pagination changes

--- pages/users/[id]/bids/placed.tsx
+++ pages/users/[id]/bids/placed.tsx
@@ -18,7 +18,6 @@ import {
 import { useIsLoggedIn } from '@liteflow/react'
 import { HiOutlineSearch } from '@react-icons/all-files/hi/HiOutlineSearch'
 import { NextPage } from 'next'
-import Trans from 'next-translate/Trans'
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo } from 'react'
@@ -63,12 +62,7 @@ const BidPlacedPage: NextPage<Props> = ({ now }) => {
   const ownerLoggedIn = useIsLoggedIn(userAddress)
 
   const date = useMemo(() => new Date(now), [now])
-  const {
-    data,
-    refetch,
-    loading: _loading,
-    previousData,
-  } = useFetchUserBidsPlacedQuery({
+  const { data, refetch } = useFetchUserBidsPlacedQuery({
     variables: {
       address: userAddress,
       limit,
@@ -77,16 +71,13 @@ const BidPlacedPage: NextPage<Props> = ({ now }) => {
     },
   })
 
-  const bidData = useMemo(() => data || previousData, [data, previousData])
-  const loading = _loading && !bidData
-
   const bids = useMemo(
     () =>
-      (bidData?.bids?.nodes || []).map((x) => ({
+      (data?.bids?.nodes || []).map((x) => ({
         ...convertBidFull(x),
         asset: x.asset,
       })),
-    [bidData],
+    [data?.bids?.nodes],
   )
 
   const onCanceled = useCallback(async () => {
@@ -103,6 +94,17 @@ const BidPlacedPage: NextPage<Props> = ({ now }) => {
     },
     [replace, pathname, query],
   )
+
+  const hasNextPage = useMemo(
+    () => data?.bids?.pageInfo.hasNextPage,
+    [data?.bids?.pageInfo.hasNextPage],
+  )
+
+  const hasPreviousPage = useMemo(
+    () => data?.bids?.pageInfo.hasPreviousPage,
+    [data?.bids?.pageInfo.hasPreviousPage],
+  )
+
   return (
     <LargeLayout>
       <UserProfileTemplate
@@ -173,15 +175,9 @@ const BidPlacedPage: NextPage<Props> = ({ now }) => {
             </Box>
           </Flex>
 
-          {loading ? (
+          {data === undefined ? (
             <Loader />
-          ) : bids.length == 0 ? (
-            <Empty
-              icon={<Icon as={HiOutlineSearch} w={8} h={8} color="gray.400" />}
-              title={t('user.bid-placed.table.empty.title')}
-              description={t('user.bid-placed.table.empty.description')}
-            />
-          ) : (
+          ) : data.bids && data.bids.nodes.length > 0 ? (
             <TableContainer bg="white" shadow="base" rounded="lg">
               <Table>
                 <Thead>
@@ -291,31 +287,21 @@ const BidPlacedPage: NextPage<Props> = ({ now }) => {
                 </Tbody>
               </Table>
             </TableContainer>
+          ) : (
+            <Empty
+              icon={<Icon as={HiOutlineSearch} w={8} h={8} color="gray.400" />}
+              title={t('user.bid-placed.table.empty.title')}
+              description={t('user.bid-placed.table.empty.description')}
+            />
           )}
-
           <Pagination
             limit={limit}
             limits={[environment.PAGINATION_LIMIT, 24, 36, 48]}
-            onLimitChange={changeLimit}
-            onPageChange={changePage}
             page={page}
-            total={bidData?.bids?.totalCount}
-            isLoading={loading}
-            result={{
-              label: t('pagination.result.label'),
-              caption: (props) => (
-                <Trans
-                  ns="templates"
-                  i18nKey="pagination.result.caption"
-                  values={props}
-                  components={[
-                    <Text as="span" color="brand.black" key="text" />,
-                  ]}
-                />
-              ),
-              pages: (props) =>
-                t('pagination.result.pages', { count: props.total }),
-            }}
+            onPageChange={changePage}
+            onLimitChange={changeLimit}
+            hasNextPage={hasNextPage}
+            hasPreviousPage={hasPreviousPage}
           />
         </Stack>
       </UserProfileTemplate>
