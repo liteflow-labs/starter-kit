cf318703adf1a32ad67061197a2ea4e9a3362939
ismailToyran <ismailtoyran@gmail.com>
2023-08-28 10:32:50 +0300

    Locales added to MintForm Ended component

--- components/Drop/MintForm/Ended.tsx
+++ components/Drop/MintForm/Ended.tsx
@@ -7,6 +7,7 @@ import {
   Text,
 } from '@chakra-ui/react'
 import BigNumber from 'bignumber.js'
+import useTranslation from 'next-translate/useTranslation'
 import numbro from 'numbro'
 import { FC, JSX, useMemo } from 'react'
 import Link from '../../Link/Link'
@@ -22,6 +23,8 @@ type Props = {
 }
 
 const MintFormEnded: FC<Props> = ({ collection, drops }): JSX.Element => {
+  const { t } = useTranslation('components')
+
   const totalMinted = useMemo(
     () =>
       drops.reduce((acc, drop) => {
@@ -29,6 +32,7 @@ const MintFormEnded: FC<Props> = ({ collection, drops }): JSX.Element => {
       }, 0),
     [drops],
   )
+
   return (
     <Box
       borderWidth="1px"
@@ -45,16 +49,17 @@ const MintFormEnded: FC<Props> = ({ collection, drops }): JSX.Element => {
         alignItems="center"
       >
         <Box gap={1} width="full">
-          <Heading variant="heading2">Mint ended</Heading>
+          <Heading variant="heading2">{t('drop.form.ended.title')}</Heading>
           <HStack alignItems="center" spacing={1}>
             <Text variant="subtitle2" color="gray.500" mr={2}>
-              Minted:
+              {t('drop.form.ended.minted')}
             </Text>
             <Text variant="subtitle2">
-              {numbro(totalMinted).format({
-                thousandSeparated: true,
-              })}{' '}
-              items
+              {t('drop.form.ended.totalMinted', {
+                totalMinted: numbro(totalMinted).format({
+                  thousandSeparated: true,
+                }),
+              })}
             </Text>
           </HStack>
         </Box>
@@ -62,7 +67,7 @@ const MintFormEnded: FC<Props> = ({ collection, drops }): JSX.Element => {
           as={Link}
           href={`/collection/${collection.chainId}/${collection.address}`}
         >
-          View collection
+          {t('drop.form.ended.viewCollection')}
         </Button>
       </SimpleGrid>
     </Box>
