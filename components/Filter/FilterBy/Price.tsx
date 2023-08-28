c6bd37d1509a7f3c2df398692274bda4bc9167e4
ismailToyran <ismailtoyran@gmail.com>
2023-08-28 14:26:44 +0300

    Display name of the currency in selectors

--- components/Filter/FilterBy/Price.tsx
+++ components/Filter/FilterBy/Price.tsx
@@ -110,8 +110,9 @@ const FilterByPrice: FC<Props> = ({
               placeholder={t('filters.assets.currency.placeholder')}
               choices={currencies.map((x) => ({
                 value: x,
-                label: x.symbol || '',
+                label: x.symbol,
                 image: x.image,
+                caption: x.name,
               }))}
               value={currency}
               required
