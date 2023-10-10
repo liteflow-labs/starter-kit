0d893f03cc79ae85f8ff5daabd4b547c55e7a422
ismailToyran <ismailtoyran@gmail.com>
2023-10-11 00:52:17 +0300

    Remove convert.ts file

--- convert.ts
+++
@@ -1,7 +0,0 @@
-// Temporary file to centralize all the conversions.
-// This file will be removed when the whole migration of the components is finalized
-
-export type FileDef = {
-  url: string
-  mimetype: string | null
-}
