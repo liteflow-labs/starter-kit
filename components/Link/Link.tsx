3dd270f741418eb3419fbe74ee177a199cd1f459
ismailToyran <ismailtoyran@gmail.com>
2023-11-06 14:11:58 +0300

    Better prop type

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -2,20 +2,18 @@ import { Link as ChakraLink, Flex, LinkProps } from '@chakra-ui/react'
 import { HiOutlineExternalLink } from '@react-icons/all-files/hi/HiOutlineExternalLink'
 import NextLink, { LinkProps as NextLinkProps } from 'next/link'
 import { forwardRef } from 'react'
-import invariant from 'ts-invariant'
 
 type IProps = Omit<LinkProps, 'href'> &
-  NextLinkProps & {
+  Omit<NextLinkProps, 'href'> & {
     externalIcon?: boolean
-  }
+  } & (
+    | { isExternal: true; href: LinkProps['href'] }
+    | { isExternal?: false; href: NextLinkProps['href'] }
+  )
 
 const Link = forwardRef<any, IProps>(function Link(props, ref) {
   const { children, href, isExternal, externalIcon, ...rest } = props
   if (isExternal) {
-    invariant(
-      typeof href === 'string',
-      'If you provide `isExternal` prop, `href` must be a string',
-    )
     return (
       <ChakraLink
         ref={ref}
