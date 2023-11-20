8798e2f0df838cfa4e042d8599db7774129df174
antho1404 <anthony.estebe@gmail.com>
2023-11-20 11:43:39 +0700

    update success/error screens

--- components/Cart/Step/Success.tsx
+++ components/Cart/Step/Success.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Icon, Stack } from '@chakra-ui/react'
+import { DrawerBody, Flex, Heading, Icon, Stack } from '@chakra-ui/react'
 import { FaCheck } from '@react-icons/all-files/fa/FaCheck'
 import useTranslation from 'next-translate/useTranslation'
 import { FC } from 'react'
@@ -10,41 +10,43 @@ type Props = {
   transactionHash: string
 }
 
-const CartSuccessStep: FC<Props> = ({ chainId, transactionHash }) => {
+const CartStepSuccess: FC<Props> = ({ chainId, transactionHash }) => {
   const { t } = useTranslation('components')
   const blockExplorer = useBlockExplorer(chainId)
   return (
-    <Stack height="full" align="center" justify="center" spacing={4}>
-      <Flex
-        as="span"
-        bgColor="green.50"
-        h={12}
-        w={12}
-        align="center"
-        justify="center"
-        rounded="full"
-      >
-        {<Icon as={FaCheck} w={8} h={8} color="green.400" />}
-      </Flex>
-      <Stack spacing={1} textAlign="center">
-        <Heading as="h3" variant="heading1" color="brand.black">
-          {t('cart.step.success.title')}
-        </Heading>
-        <Heading as="h5" variant="heading3" color="gray.500">
-          {t('cart.step.success.description')}
-        </Heading>
-        <Link
-          href={blockExplorer.transaction(transactionHash) || ''}
-          isExternal
-          _hover={{
-            textDecoration: 'underline',
-          }}
+    <DrawerBody py={4} px={2}>
+      <Stack height="full" align="center" justify="center" spacing={4}>
+        <Flex
+          as="span"
+          bgColor="green.50"
+          h={12}
+          w={12}
+          align="center"
+          justify="center"
+          rounded="full"
         >
-          {t('cart.step.success.action')}
-        </Link>
+          {<Icon as={FaCheck} w={8} h={8} color="green.400" />}
+        </Flex>
+        <Stack spacing={1} textAlign="center">
+          <Heading as="h3" variant="heading1" color="brand.black">
+            {t('cart.step.success.title')}
+          </Heading>
+          <Heading as="h5" variant="heading3" color="gray.500">
+            {t('cart.step.success.description')}
+          </Heading>
+          <Link
+            href={blockExplorer.transaction(transactionHash) || ''}
+            isExternal
+            _hover={{
+              textDecoration: 'underline',
+            }}
+          >
+            {t('cart.step.success.action')}
+          </Link>
+        </Stack>
       </Stack>
-    </Stack>
+    </DrawerBody>
   )
 }
 
-export default CartSuccessStep
+export default CartStepSuccess
