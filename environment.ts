48c0fb8991b4ae779c553983e1598feb4180b176
Nicolas Mahe <nicolas@mahe.me>
2024-01-16 12:39:02 +0700

    add neon evm config

--- environment.ts
+++ environment.ts
@@ -261,6 +261,37 @@ const getEnvironment = async (): Promise<Environment> => {
         },
       },
       {
+        id: 245022934,
+        network: 'neonMainnet',
+        name: 'Neon EVM MainNet',
+        nativeCurrency: {
+          name: 'NEON',
+          symbol: 'NEON',
+          decimals: 18,
+        },
+        rpcUrls: {
+          default: {
+            http: ['https://neon-proxy-mainnet.solana.p2p.org'],
+          },
+          public: {
+            http: ['https://neon-proxy-mainnet.solana.p2p.org'],
+          },
+        },
+        blockExplorers: {
+          default: {
+            name: 'Neonscan',
+            url: 'https://neonscan.org',
+          },
+        },
+        contracts: {
+          multicall3: {
+            address: '0xca11bde05977b3631167028862be2a173976ca11',
+            blockCreated: 206545524,
+          },
+        },
+        testnet: false,
+      },
+      {
         id: 245022926,
         network: 'neonDevnet',
         name: 'Neon EVM DevNet',
