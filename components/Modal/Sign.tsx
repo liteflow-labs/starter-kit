a751c107189923e9e2bc9a57ddf47ba13546a61c
ismailToyran <ismailtoyran@gmail.com>
2024-02-01 13:30:32 +0300

    Add loader to sign button

--- components/Modal/Sign.tsx
+++ components/Modal/Sign.tsx
@@ -13,7 +13,7 @@ import {
 } from '@chakra-ui/react'
 import { FaFileSignature } from '@react-icons/all-files/fa/FaFileSignature'
 import useTranslation from 'next-translate/useTranslation'
-import { FC, useCallback } from 'react'
+import { FC, useCallback, useState } from 'react'
 import { Connector, useDisconnect } from 'wagmi'
 import useAccount from '../../hooks/useAccount'
 
@@ -27,15 +27,18 @@ const SignModal: FC<Props> = ({ connector, isOpen, onClose }) => {
   const { t } = useTranslation('components')
   const { sign } = useAccount()
   const { disconnect } = useDisconnect()
+  const [isLoading, setLoading] = useState(false)
 
   const onSign = useCallback(async () => {
     if (!connector) return
+    setLoading(true)
     try {
       await sign(connector)
     } catch (e: any) {
       disconnect()
     } finally {
       onClose()
+      setLoading(false)
     }
   }, [connector, disconnect, onClose, sign])
 
@@ -63,7 +66,7 @@ const SignModal: FC<Props> = ({ connector, isOpen, onClose }) => {
           <Text variant="text-sm">{t('modal.signature.description')}</Text>
         </ModalBody>
         <ModalFooter as="div">
-          <Button onClick={onSign} mx="auto" size="md">
+          <Button onClick={onSign} isLoading={isLoading} mx="auto" size="md">
             {t('modal.signature.action')}
           </Button>
         </ModalFooter>
